8a918d25d4e5e196799259b286178a2d02d3956f
James Bennett <james@b-list.org>
2009-05-08 23:45:33 -0400

    Don't actually need all that junk in the setup.py.

--- setup.py
+++ setup.py
@@ -1,38 +1,14 @@
 from distutils.core import setup
-import os
-
-# Compile the list of packages available, because distutils doesn't have
-# an easy way to do this.
-packages, data_files = [], []
-root_dir = os.path.dirname(__file__)
-if root_dir:
-    os.chdir(root_dir)
-
-for dirpath, dirnames, filenames in os.walk('flashpolicies'):
-    # Ignore dirnames that start with '.'
-    for i, dirname in enumerate(dirnames):
-        if dirname.startswith('.'): del dirnames[i]
-    if '__init__.py' in filenames:
-        pkg = dirpath.replace(os.path.sep, '.')
-        if os.path.altsep:
-            pkg = pkg.replace(os.path.altsep, '.')
-        packages.append(pkg)
-    elif filenames:
-        prefix = dirpath[13:] # Strip "flashpolicies/" or "flashpolicies\"
-        for f in filenames:
-            data_files.append(os.path.join(prefix, f))
 
 
 setup(name='django-flashpolicies',
-      version='1.1',
+      version='1.2',
       description='Flash cross-domain policies for Django sites',
       author='James Bennett',
       author_email='james@b-list.org',
       url='http://bitbucket.org/ubernostrum/django-flashpolicies/overview/',
-      download_url='http://bitbucket.org/ubernostrum/django-flashpolicies/downloads/django-flashpolicies-1.1.tar.gz',
-      package_dir={'flashpolicies': 'flashpolicies'},
-      packages=packages,
-      package_data={'flashpolicies': data_files},
+      download_url='http://bitbucket.org/ubernostrum/django-flashpolicies/downloads/django-flashpolicies-1.1.tar.gz', 
+      packages=['flashpolicies'],
       classifiers=['Development Status :: 5 - Production/Stable',
                    'Environment :: Web Environment',
                    'Framework :: Django',
