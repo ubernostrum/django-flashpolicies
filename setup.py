10588fc5caa881a82d78449e29216a68e8b12074
James Bennett <james@b-list.org>
2009-06-27 22:57:40 -0500

    Make the packaging process include the tests.

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ import os
 
 
 setup(name='django-flashpolicies',
-      version='1.3',
+      version='1.3.1',
       description='Flash cross-domain policies for Django sites',
       long_description=open(os.path.join(os.path.dirname(__file__), 'README')).read(),
       author='James Bennett',
       author_email='james@b-list.org',
       url='http://bitbucket.org/ubernostrum/django-flashpolicies/overview/',
       download_url='http://bitbucket.org/ubernostrum/django-flashpolicies/downloads/django-flashpolicies-1.3.tar.gz', 
-      packages=['flashpolicies'],
+      packages=['flashpolicies', 'flashpolicies.tests'],
       classifiers=['Development Status :: 5 - Production/Stable',
                    'Environment :: Web Environment',
                    'Framework :: Django',
