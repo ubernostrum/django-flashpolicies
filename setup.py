828ebb5572705c7f269dfd9e35655df304beee34
James Bennett <james@b-list.org>
2010-03-14 05:02:40 -0500

    Call it 1.4.1.

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ import os
 
 
 setup(name='django-flashpolicies',
-      version='1.4',
+      version='1.4.1',
       description='Flash cross-domain policies for Django sites',
       long_description=open(os.path.join(os.path.dirname(__file__), 'README')).read(),
       author='James Bennett',
       author_email='james@b-list.org',
       url='http://bitbucket.org/ubernostrum/django-flashpolicies/overview/',
-      download_url='http://bitbucket.org/ubernostrum/django-flashpolicies/downloads/django-flashpolicies-1.4.tar.gz', 
+      download_url='http://bitbucket.org/ubernostrum/django-flashpolicies/downloads/django-flashpolicies-1.4.1.tar.gz', 
       packages=['flashpolicies', 'flashpolicies.tests'],
       classifiers=['Development Status :: 5 - Production/Stable',
                    'Environment :: Web Environment',
